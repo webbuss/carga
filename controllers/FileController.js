cadf67e25f73d61b5e9d04dd96a19a40833e8d93
Nestor Castillo Vega <ncastillov@gmail.com>
2022-08-03 14:08:12 -0600

    Add files via upload

---
+++ controllers/FileController.js
@@ -0,0 +1,30 @@
+class FileController
+{
+  subirArchivo = async (req, res, next) =>
+  {
+    const archivo = req.files.archivo;
+    const fileName = archivo.name;
+    const path = __dirname + '/../uploads/' + fileName;
+
+    try {
+      archivo.mv(path, (error) => {
+        if (error) {
+          console.error(error);
+          res.writeHead(500, {
+            'Content-Type': 'application/json'
+          });
+          res.end(JSON.stringify({ status: 'error', message: error }));
+            return;
+          }
+          return res.status(200).send({ status: 'success', path:'/uploads/' + fileName });
+       });
+     } catch (e) {
+       res.status(500).json({
+         error: true,
+         message: e.toString()
+       });
+     }
+  }
+}
+
+module.exports = FileController;
